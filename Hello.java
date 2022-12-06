d277c481bbfb933f8600eb6eeb661c260480bae9
shibli <shibli.shadik@gmail.com>
2022-12-06 16:50:16 +0600

    Added a new method called disply to Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,12 @@ public class Hello
     public static void main(String[] args)
     {
         System.out.println("VS code is awesome");
+
+        greeting("Shibli");
+    }
+
+    public static void greeting(String name)
+    {
+        System.out.println("Welcome " + name);
     }
 }
